b8aa82964742fc574ccea5f7f593c6f0b7f1205a
ca2longoria <ca2longoria@gmail.com>
2012-05-07 22:23:47 -0500

    A few changes to tojson.py, and two testpath files.

--- py/tojson.py
+++ py/tojson.py
@@ -3,17 +3,22 @@ def _tojson(a,b,sarr):
 	sa = len(a)
 	sb = len(b)
 	if (sa < sb):
-		for f in sa:
-			sarr.append('"f":[')
+		for i in range(sb-sa-1):
+			ind = len(a)-1-(sb-sa)+i
+			sarr.append('"'+str(a[ind])+'":[')
 	elif (sa > sb):
-		
+		for i in range(sa-sb):
+			ind = len(b)-1-(sa-sb)+i
+			sarr.append('], ')
+	return sarr
 
 def tojson(lines,c='/'):
 	arr = []
 	for i in range(1,len(lines)):
 		a = lines[i-1].split(c)
 		b = lines[i].split(c)
-	
+		_tojson(a,b,arr)
+	print ''.join(arr[:])
 
 import sys
 import platform
